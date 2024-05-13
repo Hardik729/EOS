79fffba00ccdca7d54c75632cff40e00996c6848
Hardik729 <hardik.16beecg007@gmail.com>
2024-05-13 17:54:53 +0530

    Changes made in Assignment 1

--- Day_08/Day8_1.c
+++ Day_08/Day8_1.c
@@ -4,55 +4,46 @@
 
 int main()
 {
-	int ret1,ret2,ret3,ret4,ret5,child_pid,s;
-	ret1=fork();
-	if(ret1==0)
+int ret1,ret2,ret3,i,s,child_pid;
+
+ret1 = fork();
+
+if(ret1 == 0)
 	{
-		for(int i=0;i<5;i++)
-		{
-			printf("Process ID: %d and count: %d\n",getpid(),i);
-		}
-		_exit(0);
+	for(int i=1; i<=5; i++)
+        	{
+                printf("child1: %d\n", i);
+                sleep(1);
+                }
+                _exit(0);
 	}
-	ret2=fork();
-	if(ret2==0)
-	{
-		for(int i=0;i<5;i++)
-		{
-			printf("Process ID: %d and count: %d\n",getpid(),i);
-		}
-		_exit(0);
+ret2 = fork();
+if(ret2 == 0)
+        {
+        for(int i=1; i<=5; i++)
+                {
+                printf("child2: %d\n", i);
+                sleep(1);
+                }
+                _exit(0);
+        }
+ret3 = fork();
+if(ret3 == 0)
+        {
+        for(int i=1; i<=5; i++)
+                {
+                printf("child3: %d\n", i);
+                sleep(1);
+                }
+                _exit(0);
+        }
+for(i=1; i<=3; i++)
+	{	
+	child_pid = wait(&s);
+	printf("child pid: %d\n",child_pid);
 	}
-	ret3=fork();
-	if(ret3==0)
-	{
-		for(int i=0;i<5;i++)
-		{
-			printf("Process ID: %d and count: %d\n",getpid(),i);
-		}
-		_exit(0);
-	}
-	ret4=fork();
-	if(ret4==0)
-	{
-		for(int i=0;i<5;i++)
-		{
-			printf("Process ID: %d and count: %d\n",getpid(),i);
-		}
-		_exit(0);
-	}
-	ret5=fork();
-	if(ret5==0)
-	{
-		for(int i=0;i<5;i++)
-		{
-			printf("Process ID: %d and count: %d\n",getpid(),i);
-		}
-		_exit(0);
-	}
-	for(int i=1;i<=5;i++)
-	{
-		child_pid = wait(&s);
-		printf("Child exiting PID: %d and status: %d\n",child_pid,WEXITSTATUS(s));
-	}
+	printf("parent: pid=%d\n", getpid());
+	printf("parent: parent pid=%d\n", getppid());
+
+return 0;
 }
